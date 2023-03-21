61b3a9191267d6fc12c87d99e7b2e5c02cf816c9
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-21 18:42:41 +0000

    upper and lower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * isalpha - shows 1 if character is lowercase or uppercase, another case, shows 0
+ * c: The character in ASCII code
+ * returns 1 if c is uppercase or lowercase
+ * returns 0 if otherwise
+*/
+int _isalpha(int c)
+{
+int j;
+if (c >= 97 && c <= 122){
+    if (j >= 101 && j <= 132)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
+_putchar('\n');
+}
