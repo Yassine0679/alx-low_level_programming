ebf4471e72d9e958ff61a2d52e32ddc3c8a80579
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-21 17:21:12 +0000

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Prints the alphabet in lowercase 10 times
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  * Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
@@ -13,6 +13,6 @@ for (i = 97; i <= 122; i++)
 {
 _putchar(i);
 }
-}
 _putchar('\n');
+}
 }
