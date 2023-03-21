f28ba59616db0f1a0a25e56777d8269a4b2ae3bd
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-21 17:04:04 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n = 97;
+
+    while (n <= 122)
+    {
+        _putchar(n);
+        n++;
+    }
+    _putchar('\n');
+    return (0);
+}
