df6b6150669a255af4465948ea6932f0e0b069f7
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-23 23:22:02 +0000

    multiplie

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - A funtion that multiplies two integers
+ * @a: int to be multiplied
+ * @b: int to be multiplied
+*/
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+_putchar(c);
+}
