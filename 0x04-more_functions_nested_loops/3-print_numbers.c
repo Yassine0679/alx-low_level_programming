6f7016deb819ce286d13adc2051c99bce6050d4a
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-23 23:53:44 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number from 0 to 9
+ * Return: (0) success
+*/
+void print_numbers(void)
+{
+for (i = 0; i <= 9; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return(0);
+}
