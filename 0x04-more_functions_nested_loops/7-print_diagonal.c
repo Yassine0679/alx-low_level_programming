df23fba24e15a358a6fda0943861de77db35714e
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-24 01:05:20 +0000

    correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,18 @@ _putchar('\n');
 else
 {
 int i, j;
-for (i = 1; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 for (j = 0; j < n; j++)
 {
-if (j ==i)
+if (j == i)
+{
 _putchar('\\');
+}
 else if (j < i)
+{
 _putchar(' ');
+}
 }
 }
 _putchar('\n');
