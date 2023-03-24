578f1e985a7450bbda5842a5d9d137a2a03c2ac4
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-24 00:35:22 +0000

    multiply

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
  * more_numbers - prints number from 0 to 14, 10 times
- * Return: void
 */
 
 void more_numbers(void)
 {
 int j;
 char c;
-for (j = 0; j <= 10; j++)
+for (j = 1; j <= 10; j++)
 {
 for (c = '0'; c <= '14'; c++)
 {
