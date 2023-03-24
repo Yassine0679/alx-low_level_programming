d674964797bf8647a879562dfd1bb6ca8276abaa
Yassine0679 <yassinechergaoui0@gmail.com>
2023-03-24 01:02:51 +0000

    straight line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,10 +11,16 @@ _putchar('\n');
 }
 else
 {
-int i;
+int i, j;
 for (i = 1; i <= n; i++)
 {
+for (j = 0; j < n; j++)
+{
+if (j ==i)
 _putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
 }
 _putchar('\n');
 }
